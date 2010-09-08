9cfd2b9d10fd7f2d60e9009b3a357a3c1fa85335
Kenneth Reitz <me@kennethreitz.com>
2010-09-08 15:50:44 -0400

    Basic structure.

---
+++ backuplib/cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import plac
+
+def start():
+	"""Dispatch backitup commandline application."""
+	pass
+	
+if __name__ == '__main__':
+	start()
---
+++ backuplib/core.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__name__ = 'backitup'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__license__ = 'MIT'
+__author__ = 'Kenneth Reitz & Mike X. O\'Connell'
+__copyright__ = '(c) 2010 Kenneth Reitz'
